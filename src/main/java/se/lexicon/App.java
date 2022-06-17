d2a93d628aadb4cb053018f1b08e3c6b458d83bc
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-06-17 11:10:54 +0200

    part 1 is done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,20 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Array Length: " + NameRepository.getSize());
+
+    String data[] = {"Mehrdad Javan", "Simon Elbrink", "Aso Jonsson"};
+    Arrays.sort(data, String.CASE_INSENSITIVE_ORDER);
+    NameRepository.setNames(data);
+    System.out.println("Array Length: " + NameRepository.getSize());
+    System.out.println("Print Array Data: " + Arrays.toString(NameRepository.findAll()));
+
+  }
+
 }
